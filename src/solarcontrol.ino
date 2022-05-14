dd42e69e0c4ec31b085df8fef23ae8f2b1724278
farblos <jens.schmidt001@arcor.de>
2022-05-14 18:31:35 +0200

    Added fist implementation of automatic pump control.

--- src/solarcontrol.ino
+++ src/solarcontrol.ino
@@ -19,50 +19,86 @@
 #define PUMP_RELAIS_L_OUTPUT    8
 #define AUX_RELAIS_OUTPUT       9
 
+OneWire onewire( ONE_WIRE_BUS );
+
 // Sensor 1: 28AA  ACDB  3C14  01E6 (Vorlauf)
 // Sensor 2: 28AA  E912  3D14  01DC (Rücklauf)
 // Sensor 3: 28AA  3311  3D14  01B7 (Speicher)
-
-OneWire onewire( ONE_WIRE_BUS );
-
 DallasTemperature sensors( &onewire );
 
 LiquidCrystal_I2C lcd( 0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE );
 
 RTC_DS1307 rtc;
 
-bool logp;
+#define STARTING  's'
+#define FORCE_ON  '1'
+#define FORCE_OFF '0'
+#define PUMPING   'p'
+#define WAITING   'w'
 
-volatile bool pumpp;
+#define WAIT_INIT 3000
+#define PUMP_INIT 150
+
+volatile char state;
+volatile int  waitc;
+volatile int  pumpc;
 
 void onbutton()
 {
-  static unsigned long last_interrupt_time = 0;
-  unsigned long interrupt_time = millis();
+  static unsigned long lits = 0;
+  unsigned long its = millis();
 
-  // if interrupts come faster than one second, assume it is a
-  // bounce and ignore
-  if ( interrupt_time - last_interrupt_time > 1000 ) {
-    pumpp = ! pumpp;
-    last_interrupt_time = interrupt_time;
+  // if interrupts come too fast assume that they are caused by
+  // bounces and ignore them.  It seems that the delay in the
+  // main loop does not flush (implicitly nor explicitly) pending
+  // interrupts caused by bounces, so the timespan used here has
+  // to be significantly larger than the delay.
+  if ( its - lits > 10000 ) {
+    switch ( state ) {
+    case STARTING:
+      break;
+
+    case FORCE_ON:
+      state = FORCE_OFF;
+      break;
+
+    case FORCE_OFF:
+      state = PUMPING;
+      pumpc = PUMP_INIT;
+      break;
+
+    case WAITING:
+      state = FORCE_ON;
+      break;
+
+    case PUMPING:
+      state = FORCE_OFF;
+      break;
+    }
+
+    lits  = its;
   }
 }
 
-void csvline( int temp1, int temp2, int temp3, int light, bool pumpp )
+void csvline( int tempv, int tempr, int temps, int light )
 {
   File dataFile = SD.open( "log.csv", FILE_WRITE );
   if ( dataFile ) {
     dataFile.print( rtc.now().unixtime() );
     dataFile.print( "," );
-    dataFile.print( temp1 );
+    dataFile.print( tempv );
     dataFile.print( "," );
-    dataFile.print( temp2 );
+    dataFile.print( tempr );
     dataFile.print( "," );
-    dataFile.print( temp3 );
+    dataFile.print( temps );
     dataFile.print( "," );
     dataFile.print( light );
     dataFile.print( "," );
-    dataFile.print( pumpp );
+    dataFile.print( state );
+    dataFile.print( "," );
+    dataFile.print( waitc );
+    dataFile.print( "," );
+    dataFile.print( pumpc );
     dataFile.println();
     dataFile.close();
   }
@@ -79,57 +115,93 @@ void setup()
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 
   SD.begin();
-  logp = false;
-
-  // initialize pump state and pump relais MOS-FET
-  pinMode( AUX_RELAIS_OUTPUT, OUTPUT );
-  pumpp = false;
-  digitalWrite( AUX_RELAIS_OUTPUT, HIGH );
 
   // activate internal pull-up on pump button and attach
   // interrupt to it
   pinMode( PUMP_BUTTON_INPUT, INPUT_PULLUP );
   attachInterrupt( digitalPinToInterrupt( PUMP_BUTTON_INPUT ),
-                   onbutton, FALLING );
+                   onbutton, RISING );
+
+  // initialize pump relais MOS-FET
+  pinMode( AUX_RELAIS_OUTPUT, OUTPUT );
+  digitalWrite( AUX_RELAIS_OUTPUT, HIGH );
+
+  // initialize state
+  state = STARTING;
 
   // write initialization marker to CSV
-  csvline( 0, 0, 0, 0, 0 );
+  csvline( 0, 0, 0, 0 );
 }
 
 void loop()
 {
   sensors.requestTemperatures();
-  int temp1 = sensors.getTempCByIndex( 0 ) * 100.0;
-  int temp2 = sensors.getTempCByIndex( 1 ) * 100.0;
-  int temp3 = sensors.getTempCByIndex( 2 ) * 100.0;
+  int tempv = sensors.getTempCByIndex( 0 ) * 100.0;
+  int tempr = sensors.getTempCByIndex( 1 ) * 100.0;
+  int temps = sensors.getTempCByIndex( 2 ) * 100.0;
 
   int light = analogRead( LIGHT_SENSOR_INPUT );
 
+  noInterrupts();
+  switch ( state ) {
+  case STARTING:
+    if ( (tempv != tempr) || (tempr != temps) ) {
+      state = PUMPING;
+      pumpc = PUMP_INIT;
+    }
+    break;
+
+  case FORCE_ON:
+    break;
+
+  case FORCE_OFF:
+    break;
+
+  case WAITING:
+    if ( waitc > 0 ) {
+      waitc -= light / 128;
+    }
+    else {
+      state = PUMPING;
+      pumpc = PUMP_INIT;
+    }
+    break;
+
+  case PUMPING:
+    if ( pumpc > 0 ) {
+      pumpc--;
+    }
+    else if ( tempv - temps <= 500 ) {
+      state = WAITING;
+      waitc = WAIT_INIT;
+    }
+    break;
+  }
+  interrupts();
+
+  // update display
   lcd.clear();
   lcd.setCursor( 0, 0 );
   lcd.print( "V:" );
-  lcd.print( temp1 );
+  lcd.print( tempv );
   lcd.setCursor( 7, 0 );
   lcd.print( "R:" );
-  lcd.print( temp2 );
+  lcd.print( tempr );
+  lcd.setCursor( 15, 0 );
+  lcd.print( state );
   lcd.setCursor( 0, 1 );
   lcd.print( "S:" );
-  lcd.print( temp3 );
+  lcd.print( temps );
   lcd.setCursor( 7, 1 );
   lcd.print( "L:" );
   lcd.print( light );
-  lcd.setCursor( 15, 1 );
-  lcd.print( pumpp );
 
-  // start logging only when temperature readings differ
-  if ( (temp1 != temp2) || (temp2 != temp3) || (temp1 != temp3) ) {
-    logp = true;
-  }
-  if ( logp ) {
-    csvline( temp1, temp2, temp3, light, pumpp );
+  // start logging only after completing start-up
+  if ( state != STARTING ) {
+    csvline( tempv, tempr, temps, light );
   }
 
-  if ( pumpp ) {
+  if ( (state == FORCE_ON) || (state == PUMPING) ) {
     digitalWrite( AUX_RELAIS_OUTPUT, LOW );
   }
   else {
